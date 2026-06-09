bf055ddd8f6683706c885c57543cdbc04c5c3fbd
Pedro De Jesus <pedrodejesus2007@icloud.com>
2026-06-09 20:26:30 -0300

    Create Cliente class with attributes and methods

---
+++ Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+    private String cpf;
+    private String nome;
+    private String email;
+
+    public Cliente(String cpf, String nome, String email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getCpf() { return cpf; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+
+    public String toString() {
+        return "CPF: " + cpf + " | Nome: " + nome + " | Email: " + email;
+    }
+}
