e3ba2da2d0fbb8012716f12f05c4dc71bbd03d3a
Pedro De Jesus <pedrodejesus2007@icloud.com>
2026-06-09 20:27:08 -0300

    Create ItemCompra.java

---
+++ ItemCompra.java
@@ -0,0 +1,13 @@
+public class ItemCompra {
+    private Produto produto;
+    private int quantidade;
+
+    public ItemCompra(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public double getSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+}
